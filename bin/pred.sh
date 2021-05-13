c94ef6e55ad4ef67251ccb61b3e08cc550150494
vincent <>
2021-05-13 12:03:48 +0800

    up batch pred

--- bin/pred.sh
+++ bin/pred.sh
@@ -1,5 +1,5 @@
 #python -m convert yolov3.cfg model/pretrain/yolov3.weights model/pretrain/yolo.h5
-python -m yolo_pred --model model/pretrain/yolo.h5 --image \
+python -m yolo_pred --image \
   --model_path model/jd/jd.h5 \
   --classes_path model_data/jd_classes.txt \
   --anchors_path model_data/tiny_yolo_anchors.txt
